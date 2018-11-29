9d5aa7be8f360c4be8c4b36ece909d6756b31eac
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-11-28 17:35:37 -0700

    comedians show up on index page

--- db/migrate/20181128012643_create_comedians.rb
+++ db/migrate/20181128012643_create_comedians.rb
@@ -1,9 +1,11 @@
 class CreateComedians < ActiveRecord::Migration[5.2]
   def change
     create_table :comedians do |t|
-      t.text  :name
-      t.text  :age
-      t.text  :city
+      t.text     :name
+      t.text     :city
+      t.integer  :age
+
+      t.timestamps
     end
   end
 end
